49c8171018a4b0b145757fcd395873e38ac751f6
Tamara Ulrich <ta.ul@gmx.ch>
2026-05-04 16:16:29 +0200

    Initial Commit

---
+++ MBTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MBTracker
+//
+//  Created by Tamara on 04.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MBTracker/MBTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBTrackerApp.swift
+//  MBTracker
+//
+//  Created by Tamara on 04.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MBTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
